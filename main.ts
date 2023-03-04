e3352fec33193bfb99aef16e793bcbaf2f0f8dec
Marcelo Pélos <mpelos@gmail.com>
2023-03-04 13:34:14 -0300

    Simple ChatGpt integration

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { ChatCompletionRequestMessage } from "npm:openai@3.2";
+import { ChatGptClient } from "./src/chatgpt.client.ts";
+import chalk from 'npm:chalk@5';
+import process from 'node:process';
+
+const promptFn = prompt;
+
+const chatGptClient = new ChatGptClient({
+  apiKey: Deno.env.get('CHATGPT_API_KEY') as string,
+  hostname: 'api.openai.com',
+});
+const history: ChatCompletionRequestMessage[] = [];
+
+const iterate = async () => {
+  const prompt = await promptFn(chalk.gray('>'));
+
+  if (prompt) {
+    history.push({ role: 'user', content: prompt })
+    process.stdout.write('\n');
+    const response = await chatGptClient.chat(prompt, history, (chunk) => {
+      process.stdout.write(chalk.blueBright(chunk));
+    });
+    process.stdout.write('\n\n');
+    history.push({ role: 'assistant', content: response })
+  }
+
+  await iterate();
+};
+
+(async () => {
+  await iterate();
+})();
---
+++ src/chatgpt.client.ts
@@ -0,0 +1,109 @@
+// deno-lint-ignore-file no-explicit-any
+import { ChatCompletionRequestMessage } from "npm:openai@3.2";
+import https from 'node:https';
+
+// #region typings
+interface ChatGptClientDependencies {
+  apiKey: string;
+  hostname: string;
+  systemMessage?: string;
+}
+// #endregion
+
+export class ChatGptClient implements ChatGptClientDependencies {
+  apiKey: string;
+  hostname: string;
+  systemMessage?: string;
+
+  constructor({
+    apiKey,
+    hostname,
+    systemMessage,
+  }: ChatGptClientDependencies) {
+    this.apiKey = apiKey;
+    this.hostname = hostname;
+    this.systemMessage = systemMessage;
+  }
+
+  chat(
+    prompt: string,
+    history: ChatCompletionRequestMessage[] = [],
+    callback?: (chunk: string) => unknown,
+  ): Promise<string> {
+    const isStream = Boolean(callback);
+
+    const systemMessage: ChatCompletionRequestMessage[] = this.systemMessage
+      ? [{ role: 'system', content: this.systemMessage }]
+      : [];
+    const messages: ChatCompletionRequestMessage[] = [
+      ...systemMessage,
+      ...history,
+      { role: 'user', content: prompt },
+    ];
+
+    return new Promise((resolve, reject) => {
+      const req = https.request(
+        {
+          method: 'POST',
+          hostname: this.hostname,
+          path: '/v1/chat/completions',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${this.apiKey}`,
+          },
+        },
+        (res) => {
+          const chunks: string[] = [];
+
+          res.on('error', (err) => reject(err));
+
+          res.on('data', (chunk) => {
+            if (isStream) {
+              chunk.toString()
+                .split('data: ')
+                .filter(Boolean)
+                .map((c: string) => {
+                  try {
+                    return JSON.parse(c.trim());
+                  } catch {
+                    return undefined;
+                  }
+                })
+                .forEach((json: any) => {
+                  if (!json) { return; }
+
+                  const messageChunk = json.choices[0].delta.content;
+
+                  if (!messageChunk) { return; }
+
+                  chunks.push(messageChunk);
+                  if (callback) { callback(messageChunk); }
+                });
+
+              chunk.toString().replace(/^data: /, '');
+            } else {
+              try {
+                const json = JSON.parse(chunk.toString());
+                const message = json.choices[0].message?.content
+                chunks.push(message);
+              // deno-lint-ignore no-empty
+              } catch { }
+            }
+          });
+
+          res.on('end', () => {
+            resolve(chunks.join(''));
+          });
+        },
+      );
+
+      req.write(JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages,
+        stream: isStream,
+      }));
+
+      req.end();
+    });
+  }
+}
