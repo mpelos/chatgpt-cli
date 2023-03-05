c7a7cd78004b0fa37219ff631ad77ba9659e9337
Marcelo Pélos <mpelos@gmail.com>
2023-03-05 11:51:29 -0300

    Multiline prompt support

--- src/chat.ts
+++ src/chat.ts
@@ -1,18 +1,35 @@
-import { ChatCompletionRequestMessage } from 'npm:openai@3.2';
 import chalk from 'npm:chalk@5';
 import process from 'node:process';
 
 import { ChatGptClient } from './chatgpt.client.ts';
+import { ChatConfig } from './config.model.ts';
 
 const promptFn = prompt;
 
 const iterate = async (
   chatGptClient: ChatGptClient,
-  history: ChatCompletionRequestMessage[] = [],
-) => {
-  const prompt = await promptFn(chalk.gray('>'));
+  history: ChatConfig['history'] = [],
+  multilinePrompt?: Array<string | undefined>,
+): Promise<{ history: ChatConfig['history'], multilinePrompt?: Array<string | undefined> }> => {
+  const lineStart = multilinePrompt ? '…' : '>';
+  let prompt = (await promptFn(chalk.gray(lineStart)) || '').trim() || undefined;
+  if (multilinePrompt) { multilinePrompt.push(prompt); }
 
-  if (prompt) {
+  // When prompt is empty it starts the multiline prompt
+  if (prompt == null && !multilinePrompt) { multilinePrompt = []; }
+
+  // When multiline prompt starts, it will end with two empty prompts
+  if (
+    prompt == null &&
+    multilinePrompt?.length &&
+    multilinePrompt.slice(-1).every(v => v == null)
+  ) {
+    prompt = multilinePrompt.join('\n');
+    multilinePrompt = undefined;
+  }
+
+  // Sends prompt to ChatGPT
+  if (prompt && !multilinePrompt) {
     history.push({ role: 'user', content: prompt })
     const response = await chatGptClient.chat(prompt, history, (chunk) => {
       process.stdout.write(chalk.blueBright(chunk));
@@ -21,22 +38,25 @@ const iterate = async (
     history.push({ role: 'assistant', content: response })
   }
 
-  return history;
+  return { history, multilinePrompt };
 };
 
 const initChat = async (
   chatGptApiKey: string,
-  history: ChatCompletionRequestMessage[] = [],
-  callback?: (history: ChatCompletionRequestMessage[]) => unknown,
+  history: ChatConfig['history'] = [],
+  callback?: (history: ChatConfig['history']) => unknown,
 ) => {
   const chatGptClient = new ChatGptClient({
     apiKey: chatGptApiKey,
     hostname: 'api.openai.com',
   });
+  let multilinePrompt: Array<string | undefined> | undefined = undefined;
 
   while (true) {
-    history = await iterate(chatGptClient, history);
-    if (callback) { callback(history); }
+    const promise = iterate(chatGptClient, history, multilinePrompt);
+    const params = await promise;
+    multilinePrompt = params.multilinePrompt;
+    if (callback) { callback(params.history); }
   }
 };
 
